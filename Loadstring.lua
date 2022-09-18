8afcf14b63d05e50fdaf6034b9df722493b32b26
LegoRblx <111660529+LegoRblx@users.noreply.github.com>
2022-09-18 15:51:50 -0700

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,13 +1,15 @@
 a = pcall(function() 
-      if shared.DNutLoaded == true then
+      if shared.DNutLoaded then
          error("Already loaded!")
          else
           loadstring(game:HttpGet("https://raw.githubusercontent.com/V0rt3xqa/Dnut/main/Dnut%20config"))()
        end
+            print(shared.DNutLoaded)
 end)
 if not a then
    warn("Failed to load")
-   end
+end
+print(shared.DNutLoaded)
 local a=syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function()end
 local callback
 callback = game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(b)if b==Enum.TeleportState.Started then 
