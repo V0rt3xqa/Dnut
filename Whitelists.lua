dbf7840c84918837a2b369303d84dab0a6ed58e7
LegoRblx <111660529+LegoRblx@users.noreply.github.com>
2022-09-19 17:12:16 -0700

    Update Whitelists.lua

--- Whitelists.lua
+++ Whitelists.lua
@@ -1,5 +1,5 @@
 return  {
-  ["82a8db09961c7009f04660052eb9cb511d5f77d75531fc99ee49a60130721b45a0d43ee2f74050ef6fee69a9eed5d6cc7cc2e1a5171546d16dbbab6e91a35f8f"]
+        ["82a8db09961c7009f04660052eb9cb511d5f77d75531fc99ee49a60130721b45a0d43ee2f74050ef6fee69a9eed5d6cc7cc2e1a5171546d16dbbab6e91a35f8f"]
 		= {"DNut Developer",Color3.new(0.333333, 0.333333, 1)},-- Lego/ Vxpe man
 	["5208cdc24c953e1b2b0db014d6693c2c1d59485d0cd1c9aeb8c682c39736cd35d60d9970643387f647a22a900c742e61c5833e2a22064e126a7a0c59fec07c80"] 
 		= {"DNut Developer",Color3.new(0.333333, 0.333333, 1)}, -- Driped out
@@ -7,4 +7,6 @@ return  {
 		= {"DNut Owner",Color3.new(0.333333, 1, 1)}, -- Owner
 	["be8a76c24a91c7c50ba45a6a2595c4f9b652d94e7b1c7158df160c5bf9ef33ccb36984fbf5fb69c7c51a2243339dcbe3fa69248f3e2c2beb00ea834121af59de"] 
 		= {"DNut Private",Color3.new(0.666667, 0, 1)}, -- Pengu
+	["237393324ebbe6c3bf28d777b78252142234117f6629dde32908a7cb345684eccf9cf99c916af16ce06b25d62a038428fa7b123b3824397a1703cbb7a06291a6 "] 
+	        = {"DNut Private",Color3.new(0.666667, 0, 1)}, -- Pengu
 }
