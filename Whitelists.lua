44abe855c90c1e7c006c2c2a080823dd889ff94c
V0rt3xqa <111151771+V0rt3xqa@users.noreply.github.com>
2023-01-24 15:21:42 -0800

    Update Whitelists.lua

--- Whitelists.lua
+++ Whitelists.lua
@@ -3,7 +3,7 @@ return  {
 		= {"DNut Developer",Color3.new(0.333333, 0.333333, 1)},-- Lego/ Vxpe man
 	["5208cdc24c953e1b2b0db014d6693c2c1d59485d0cd1c9aeb8c682c39736cd35d60d9970643387f647a22a900c742e61c5833e2a22064e126a7a0c59fec07c80"] 
 		= {"DNut Developer",Color3.new(0.333333, 0.333333, 1)}, -- Driped out
-	["4270621253"] 
+	["be8a76c24a91c7c50ba45a6a2595c4f9b652d94e7b1c7158df160c5bf9ef33ccb36984fbf5fb69c7c51a2243339dcbe3fa69248f3e2c2beb00ea834121af59de"] 
 		= {"DNut Owner",Color3.new(0.333333, 1, 1)}, -- Owner
 	["be8a76c24a91c7c50ba45a6a2595c4f9b652d94e7b1c7158df160c5bf9ef33ccb36984fbf5fb69c7c51a2243339dcbe3fa69248f3e2c2beb00ea834121af59de"] 
 		= {"DNut Private",Color3.new(0.666667, 0, 1)}, -- Pengu
