859be68119dee06f1b35b0ba051a5abcce17e5c8
ProUseryes <124409789+ProUseryes@users.noreply.github.com>
2023-03-04 16:49:39 -0800

    Update Whitelists.lua

--- Whitelists.lua
+++ Whitelists.lua
@@ -1,15 +1,15 @@
 return  {
-        ["d7a42493956c2eee54da2712b220769c0aa5bbe3714477db9949ff85db531c341a3d789fe8bafa094f22dd65f845f99b6db6b3f690c93d982e333c04faefb085"]
-		= {"DNut Developer",Color3.new(0.333333, 0.333333, 1)},-- Lego/ Vxpe man
+        ["1c90c356c943e151804033561da5f858cc38b7c0cd18cadcc2b2b4f18886f0c3da424823e28c5099ba7093e96f885e6874a932482b6970db9457bcc5b45b6458 "]
+		= {"DNUT OWNER",Color3.new(0.333333, 0.333333, 1)},-- Lego/ Vxpe man
 	["5208cdc24c953e1b2b0db014d6693c2c1d59485d0cd1c9aeb8c682c39736cd35d60d9970643387f647a22a900c742e61c5833e2a22064e126a7a0c59fec07c80"] 
-		= {"DNut Developer",Color3.new(0.333333, 0.333333, 1)}, -- Driped out
+		= {"DNUT DEVELOPER",Color3.new(0.333333, 0.333333, 1)}, -- Driped out
 	["537df31c49c846786815ad1268266669114bb626b7d93deb64ac88dc992ad0a5f077c4504524c83024ff208d9e0cdf9a5131d22c9b1c0ada5e17faa9a3af81ae"] 
-		= {"DNut Owner",Color3.new(0.333333, 1, 1)}, -- Owner
+		= {"DNUT OWNER",Color3.new(0.333333, 1, 1)}, -- Owner
 	["be8a76c24a91c7c50ba45a6a2595c4f9b652d94e7b1c7158df160c5bf9ef33ccb36984fbf5fb69c7c51a2243339dcbe3fa69248f3e2c2beb00ea834121af59de"] 
-		= {"DNut Private",Color3.new(0.666667, 0, 1)}, -- Pengu
+		= {"DNUT PRIVATE",Color3.new(0.666667, 0, 1)}, -- Pengu
 	["237393324ebbe6c3bf28d777b78252142234117f6629dde32908a7cb345684eccf9cf99c916af16ce06b25d62a038428fa7b123b3824397a1703cbb7a06291a6 "] 
-	        = {"DNut Private",Color3.new(0.666667, 0, 1)}, -- Pengu
+	        = {"DNUT PRIVATE",Color3.new(0.666667, 0, 1)}, -- Pengu
 	["66a7a647317bf00d4783952a3da946a694ad2f59d5cb4130948285dd03c29a6c8a5aaf0c0dafd8d3b9a33abf2ff407f2a7ca0cabd3bdc803e95f406cc61e6f79"]
-	        = {"DNut Private",Color3.new(0.666667, 0, 1)}, --piss boy
+	        = {"DNUT PRIVATE",Color3.new(0.666667, 0, 1)}, --piss boy
 }
 --whitelists
