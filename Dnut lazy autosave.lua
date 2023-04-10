85fdad86a165962454fc982d0b2830fa5e2ee7b2
V0rt3xqa <111151771+V0rt3xqa@users.noreply.github.com>
2023-04-10 12:44:01 -0500

    Update Dnut lazy autosave.lua

--- Dnut lazy autosave.lua
+++ Dnut lazy autosave.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://pastebin.com/raw/Rh4PbjWy"))();
-loadstring(game:HttpGet("https://raw.githubusercontent.com/V0rt3xqa/Dnut/main/Loadstring.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/V0rt3xqa/Dnut/main/Dnut%20config"))()
